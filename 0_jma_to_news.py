4f8d3d935df7b15cf9d5b3dd9ad82d73c987cfa9
hshin-git <hiromasa.shin@gmail.com>
2020-06-14 18:13:24 +0900

    add scrollX and stateSave to datatable init

--- 0_jma_to_news.py
+++ 0_jma_to_news.py
@@ -92,7 +92,7 @@ html = '''
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" />
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-<script> $(document).ready( function () { $('.dataframe').DataTable({ "columnDefs":[{"orderable":false,"targets":[4,6]}], }); } ); </script>
+<script> $(document).ready( function () { $('.dataframe').DataTable({ "scrollX":true,"stateSave":true,"columnDefs":[{"orderable":false,"targets":[4,6]}], }); } ); </script>
 </head>
 <body>
 '''
@@ -111,4 +111,3 @@ with open(JMA_HTML,'w') as f:
 ##################################################
 print("leave:",sys.argv)
 sys.exit(0)
-
